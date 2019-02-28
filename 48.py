f1a003881bf16aa9bb06468473517232066af0b4
tedmik <47427441+tedmik@users.noreply.github.com>
2019-02-28 12:12:54 -0500

    Create 48.py

---
+++ 48.py
@@ -0,0 +1,25 @@
+year = int(input("Which year?: "))
+if(year % 12 == 0 ):
+  print("It is/was the year of the monkey")
+elif(year % 12 == 1 ):
+  print("It is/was the year of the Rooster")
+elif(year % 12 == 2):
+  print("It is/was the year of the Dog")
+elif(year % 12 == 3):
+  print("It is/was the year of the Pig")
+elif(year % 12 == 4):
+  print("It is/was the year of the Rat")
+elif(year % 12 == 5):
+  print("It is/was the year of the Ox")
+elif(year % 12 == 6):
+  print("It is/was the year of the Tiger")
+elif(year % 12 == 7):
+  print("It is/was the year of the Hare")
+elif(year % 12 == 8):
+  print("It is/was the year of the Dragon")
+elif(year % 12 == 9):
+  print("It is/was the year of the Snake")
+elif(year % 12 == 10):
+  print("It is/was the year of the Horse")
+elif(year % 12 == 11):
+  print("It is/was the year of the Sheep")
