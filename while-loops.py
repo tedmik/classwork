6ba87b649dc4d1d56fe0f542fcbea24bed663d17
tedmik <47427441+tedmik@users.noreply.github.com>
2019-03-25 12:33:18 -0400

    Create while-loops.py

---
+++ while-loops.py
@@ -0,0 +1,45 @@
+
+ # 1
+my_str = "hello"
+i = 0
+while i < 5:
+  char = my_str[i]
+  print(char)
+  i += 1
+
+
+# 2
+numbers = [7, 7, 2, 7, 11]
+i = 0
+while i < 5:
+  num = numbers[i]
+  print(num)
+  i += 1
+
+# 3
+marks = [84, 89, 90, 45, 67]
+i = 0
+while i < 5:
+  mark = marks[i]
+  print(mark)
+  i += 1 
+
+# 4
+i = 0
+while i < 10:
+  print(i)
+  i += 1 
+
+# 5
+i = 5
+while i < 100:
+  print(i)
+  i += 5
+
+# 6
+i = 100
+while i > -1:
+  print(i)
+  i -= 5
+
+
